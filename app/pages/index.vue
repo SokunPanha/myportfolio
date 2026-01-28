f9af50af163c263ccf2e42f5cd5820c5e2aa1085
panha <sokunpanha.panha@gmail.com>
2026-01-28 21:22:39 +0700

    add favicon and meta data

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,6 +5,36 @@ const { elementRef: aboutRef, isVisible: aboutVisible } = useScrollReveal()
 const { elementRef: workExpRef, isVisible: workExpVisible } = useScrollReveal()
 const { elementRef: recentWorkRef, isVisible: recentWorkVisible } = useScrollReveal()
 const { elementRef: contactRef, isVisible: contactVisible } = useScrollReveal()
+useHead({
+    title: 'Panha - My Portfolio',
+    meta: [
+        {
+            name: 'description',
+            content: 'Welcome to my portfolio website where I showcase my projects and skills.'
+        },
+        {
+            name: 'keywords',
+            content: 'portfolio, projects, web development, skills, about me'
+        }
+    ],
+    link: [
+        {
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        }
+    ]
+})
+
+useSeoMeta({
+    title: 'Panha - My Portfolio',
+    description: 'Welcome to my portfolio website where I showcase my projects and skills.',
+    keywords: 'portfolio, projects, web development, skills, about me',
+    ogType: 'website',
+    ogImage: '/images/profile-image.webp',
+    twitterCard: 'summary_large_image',
+    twitterImage: '/images/profile-image.webp',    
+})
 </script>
 
 <template>
